c383a875f28efa0724e6fefb55995991893b17e7
shristi482004 <shristish484@gmail.com>
2026-03-11 19:38:25 +0530

    added about

--- lec-1/index.js
+++ lec-1/index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('This is about page');
+})
+
 app.listen(process.env.PORT,()=>{
     console.log('Server is running on port ' + process.env.PORT);
 });
